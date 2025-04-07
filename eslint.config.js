802a4999fb981bbc3ab1fe79ee2e0d67224c2df7
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2025-04-07 21:41:48 +0100

    Revert "install prettier and add configuration to eslint.config.js file"
    This reverts commit bb6f743ea418f009bb637ea061dd6bb2d7793222.

--- eslint.config.js
+++ eslint.config.js
@@ -7,13 +7,7 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "prettier"
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
